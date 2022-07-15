b189e037682df6f869ff34db7f5ea1969976a2f6
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-07-15 22:54:14 +0600

    Danoton Web Site

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// * Nabvar
-const nav = document.querySelector(".nav");
-const navOpenBtn = document.querySelector(".navbar--open--icon");
-const navCloseBtn = document.querySelector(".navbar--close--icon");
-const navLinks = document.querySelectorAll(".nav__link");
-
-navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("open");
-});
-
-navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("open");
-});
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.classList.remove("open");
-  });
-});
-
 // * Swiper Js
 var swiper = new Swiper(".hero-swiper", {
   cssMode: true,
