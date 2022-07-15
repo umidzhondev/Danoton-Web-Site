08f93a7cd0f497c8b1d003127bbff89148af40a1
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-07-15 06:58:41 +0600

    Danoton Web Site

--- js/main.js
+++ js/main.js
@@ -36,3 +36,21 @@ var swiper = new Swiper(".hero-swiper", {
     disableOnInteraction: false,
   },
 });
+var swiper = new Swiper(".applied-swiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
